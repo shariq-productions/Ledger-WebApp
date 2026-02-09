80c61cb5461b98b161a87e44d8dddb652461dbaf
shariqyb <shariq.aftab@yogyabano.com>
2026-02-10 02:27:23 +0530

    Deployed commit

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+print(f"Database URL: {settings.DATABASE_URL}")
 # Create database engine
 engine = create_engine(
     settings.DATABASE_URL,
